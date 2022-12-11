7becb00802031519b574074b7a028742f7071c0c
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2022-12-11 03:21:03 -0300

    Agregar tabla de favoritos

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 
 from apps.core.models import TimeStampedModel
+from apps.usuarios.models import Usuarios
 
 class Productos(TimeStampedModel):
     nombre = models.CharField(max_length=50, null=False, blank=False)
@@ -21,3 +22,11 @@ class Imagenes(TimeStampedModel):
         db_table = 'productos_Imagen'
         verbose_name_plural = 'Imagenes'
 
+# Realcion Usuario Producto
+class Favorito(TimeStampedModel):
+    usuario = models.ForeignKey(Usuarios, null=False, blank=False, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Productos, null=False, blank=False, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "productos_Favoritos"
+        verbose_name_plural = "Favoritos"
